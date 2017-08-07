32dd5fbd43b3fc28f678784507123ed27b78056b
Hieu Van <hieuvanphu91@gmail.com>
2017-08-08 00:18:03 +0700

    chore(swagger): setup swagger

--- src/server.js
+++ src/server.js
@@ -1,4 +1,10 @@
 const hapi = require('hapi');
+const inert = require('inert');
+const vision = require('vision');
+const swagger = require('hapi-swagger');
+
+const pkg = require('../package.json');
+const chatPlugin = require('./api/chat-plugin');
 
 const server = new hapi.Server();
 
@@ -11,7 +17,32 @@ server.connection([
   }
 ]);
 
-const plugins = [];
+const plugins = [
+  {
+    register: inert,
+    select: ['docs']
+  },
+  {
+    register: vision,
+    select: ['docs']
+  },
+  {
+    register: swagger,
+    select: ['docs'],
+    cache: { expiresIn: 24 * 60 * 60 * 1000 },
+    options: {
+      info: {
+        title: pkg.description,
+        version: pkg.version
+      },
+      documentationPath: '/docs'
+    }
+  },
+  {
+    register: chatPlugin,
+    select: ['api', 'docs', 'chat'],
+  }
+];
 
 server.register(plugins, (err) => {
   if (err) { console.error('error loading plugin ', err); }
