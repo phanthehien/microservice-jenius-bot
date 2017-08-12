a176c283eab5002bb916651d292ab2f900db7f1a
Dao Le Quang <dao.le@pycogroup.com>
2017-08-12 13:07:42 +0700

    Update config redis

--- config/redis.js
+++ config/redis.js
@@ -1,16 +1,15 @@
 // @flow
 
-const get = require('./index');
+const Config = require('./index');
 const Redis = require('redis');
 const Bluebird = require('bluebird');
 
 Bluebird.promisifyAll(Redis.RedisClient.prototype);
 Bluebird.promisifyAll(Redis.Multi.prototype);
 
-// const { hostname, port } = get('resources.config.redis');
-const redisConfig = { hostname: 'localhost', port: 6379 };
+const { hostname, port } = Config.get('/resources/redis');
 
-const client = Redis.createClient(redisConfig.port, redisConfig.hostname);
+const client = Redis.createClient(port, hostname);
 
 client.on('error', (err) => {
   console.error(`Couldn't connect to redis ${err}`);
